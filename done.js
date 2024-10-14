9e6579f43d0f414a0230d11fe724faebc78a83e7
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-14 17:48:26 +0000

    Starting with katas2

--- done.js
+++ done.js
@@ -3,55 +3,26 @@
 /* COLOR KATAS */
 
 /* No Prompt */
-/* Var Alert */
-// make a string variable named color, set it equal to your favorite color.
-var color = "red";
-// alert the message" "My favorite color is " plus the variable name outside quotes.
-alert("My favorite color is " + color);
+
 
 /* Prompt */
-/* Var Alert Prompt */
-// make a string variable for color, prompt the user for their favorite color.
-var color=prompt("whats your favorite color??");
-// alert color " plus is a nice color!"
-alert(color + " is a nice color!");
+
 
 /* Prompt with Conditional */
-var color = prompt("what is your favorite color?");
-if (color == "black" || color =="Black") alert("black is not really a color");
-else alert(color + "is a nice color");
+
 
 /* AGE */
 
 /* No Prompt */
-/* Var Alert */
-// make an integer variable for age, set it equal to your age
-var age = 16;
-// make a string variable for name, set it to your name
-var namee = "victoria";
-// alert name is age years old
-alert(namee + " is " + age + " years old");
 
 
 /* Prompt */
 /* Var Alert Prompt */
-// make an integer variable for age, prompt user for their age
-var age = prompt("how old are you?");
-// make a string variable for name, prompt user for their name
-var namee = prompt("whats your name?");
-// alert name is age years old
-alert("their name is "+ namee + " are " + age + " years old");
+
 
 /* Prompt with Conditional */
-/* Var Alert Prompt Conditional */
-// make an integer variable for age, prompt user for it
-var age = prompt("how old are you");
-// make a string variable for name, prompt user for it
-var namee = prompt("whats your name?");
-// if age is greater than 16, alert "you don't look that old!"
-if (age > 16) alert("you dont look that old!")
-// otherwise alert name is age years old
-else alert(namee + " is " + age + " years old");
+
+
 
 /* ICE CREAM */
 
