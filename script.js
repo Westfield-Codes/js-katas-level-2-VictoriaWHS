68c7258ccb572c04560bb1f7b758e8aa69b7d064
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-21 16:12:51 +0000

    not done

---
+++ script.js
@@ -0,0 +1,17 @@
+/* FUNCTION main() 
+ * Facebook wants to check if you're 13, so they ask your birth year 
+ * set age = "How old are you?"
+ * then set born = asking "What year were you born?"
+ * If myAge(age) is not equal to born, say, "Underaged", otherwise "Welcome!"
+ * @param: none (main never gets params)
+ * @return: none (main never gets returns
+ */
+// current year//
+functionmain(){
+    let age= prompt(" how old are you?");
+    let born= prompt("What year were you born?");
+    if (age == born - //current year // ) alert(" welcome!");
+        else alert("underage");
+
+
+}
