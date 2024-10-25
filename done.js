56f837ef1eb9e41e2641476eb330c74634a915ce
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-25 00:10:32 +0000

    Not working

--- done.js
+++ done.js
@@ -26,3 +26,33 @@ function myAge(age){
     let birthYear = thisYear - age;
     return birthYear;
 }
+
+/* PET KATA */
+main();
+/* Var, Alert, Prompt, Function, Global */
+
+/* FUNCTION main()  
+ * Ask for the name of a dog (dogName)
+ * Ask how many cups of food they get each meal (cups)
+ * call feedDog with parameters dogName, cups
+ * @param: none (main never gets params)
+ * @return: none (main never gets returns
+ */
+function main(){
+    let dogName = prompt("what is your dog's name?");
+    let cup = prompt("how many cups of food do thy get?")
+    feedDog(dogName, cups);
+}
+
+function feedDog(dogName, cup){
+    for (let cup = 1; cup <= cups; cup=cup+2){
+        alert("here's a cup of kibble, "+ dogName);
+    } 
+
+}
+
+/* FUNCTION feedDog(dogName, cups) */
+/* define a function called feedDog with string parameter dogName, number parameter cups
+ * loop from cup = 1 to cups.  For each cup, display "Here's a cup of kibble, [dogName]!"
+ * @param: dogName (string), cups (integer)
+ * @return: none*/
--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-main();
 
-/* AGE KATA */
-
-/* Var, Alert, Prompt, Function */
-
-/* FUNCTION main() 
- * Facebook wants to check if you're 13, so they ask your birth year 
- * set age = "How old are you?"
- * then set born = asking "What year were you born?"
- * If myAge(age) is not equal to born, say, "Underaged", otherwise "Welcome!"
+/* FUNCTION main()  
+ * Ask for the name of a dog (dogName)
+ * Ask how many cups of food they get each meal (cups)
+ * call feedDog with parameters dogName, cups
  * @param: none (main never gets params)
  * @return: none (main never gets returns
  */
-function main(){
-    let age= prompt("how old are you?");
-    let born= prompt("what year were you born");
-    if (myAge(age) != born) alert("underaged");
-        else if (myAge(age)<16) alert("underaged, below 16.");
-            else alert("welcome!")
-}
-/* FUNCTION myAge(age)
- * define a function called myAge with number parameter age
- * Get thisYear using the date function https://www.w3schools.com/js/js_date_methods.asp
- * set birthYear as thisYear minus age and return that
- * @param: age (integer)
- * @return: birthyear (year)
- */
+/* FUNCTION feedDog(dogName, cups) */
+/* define a function called feedDog with string parameter dogName, number parameter cups
+ * loop from cup = 1 to cups.  For each cup, display "Here's a cup of kibble, [dogName]!"
+ * @param: dogName (string), cups (integer)
+ * @return: none
+ */ 
 
-//define a function called myAge with number parameter age//
-function myAge(age){
-   //?//
-    let today = new Date();
- //Get this year.  Use  date function)//
-    let thisYear = today.getFullYear();
-    //set birthYear as thisYear - age//
-    let birthYear = thisYear - age;
-    //return//
-    return birthYear;
+main();
+
+function main(){ 
+    let dogName = prompt("whats your dog's name?");
+    let cups = prompt("How many cups of food do they get each meal?");
+    feedDog(dogName, cups);
 }
 
+
+function feedDog(dogName, cup){
+    for (let cup = 1; cup <= cups; cup=cups+2){
+        alert("Here's a cup of kibble, " + dogName + " !");
+    }
+}
