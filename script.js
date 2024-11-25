d0492c0a55ba5afddcce43d4f04465d2cc3853c6
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-11-25 16:52:53 +0000

    Not done

--- script.js
+++ script.js
@@ -2,46 +2,62 @@
 /* CALCULATOR KATA */
 
 /* This is best done with a switch control, though can also be a chain of conditionals. */
-main();
-function main(){
-    let operation = prompt()
+
+// FUNCTION main() 
+function main(a,b){
+    let operation = prompt("What operation do you want and make sure to spell it out, '+','-','*','/'?");
+    let a =prompt("What is the first number?");
+    let b = prompt("What is the second number?");
+    if (operation == "addition" || operation == "add") add();
+    else if (operation == "subtraction") subtract();
+    else if (operation == "division" || operation =="divide") function divide();
 }
-/* FUNCTION main() 
- * Ask the user what operation they want (+, -, *, /)
+ // Ask the user what operation they want (+, -, *, /)
 
- * Ask the user for the first number (a)
- * Parse a as an integer
- * Ask the user for the second number (b)
- * Parse b as an integer
- * Based on the operation, send the numbers to the right function as
- * arguments and store the returned value in variable called result
- * Alert the equation a + operation + b = result
- * @param: none (main never gets params)
- * @return: none (main never gets returns
- */
+//  Ask the user for the first number (a)
+//  Parse a as an integer
+ // Ask the user for the second number (b)
+ // Parse b as an integer
+ // Based on the operation, send the numbers to the right function as
+ // arguments and store the returned value in variable called result
+ // @param: none (main never gets params)
+ // @return: none (main never gets returns
+ 
 
-/* FUNCTION add(a,b)
- * define a function called add with two number parameters a and b
- * @param: a, b (integers)
- * @return: a + b
- */
+//FUNCTION add(a,b)
+function add(a,b){
 
-/* FUNCTION subtract(a,b)
- * define a function called subtract with two number parameters a and b
- * @param: a, b (integers)
- * @return: a - b
- */
+}
+// define a function called add with two number parameters a and b
+// @param: a, b (integers)
+// @return: a + b
 
-/* FUNCTION multiply(a,b)
- * define a function called multiply with two number parameters a and b
- * @param: a, b (integers)
- * @return: a * b (* means multiply)
- */
 
-/* FUNCTION divide(a,b)
- * define a function called divide with two number parameters a and b
- * @param: a, b (integers)
- * @return: a / b (/ means divided by)
- */
+// FUNCTION subtract(a,b)
+function subtract(a,b){
+
+}
+// define a function called subtract with two number parameters a and b
+// @param: a, b (integers)
+// @return: a - b
+
+
+// FUNCTION multiply(a,b)
+function multiply(a,b){
+
+}
+// define a function called multiply with two number parameters a and b
+// @param: a, b (integers)
+// @return: a * b (* means multiply)
+//
+
+// FUNCTION divide(a,b)
+function divide(a,b){
+
+}
+// define a function called divide with two number parameters a and b
+// @param: a, b (integers)
+// @return: a / b (/ means divided by)
+//
 
 
